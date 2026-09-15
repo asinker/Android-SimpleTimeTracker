463b5e02ab93f4ffda4662d2e8efe89b31cab260
asinker <asinker@users.noreply.github.com>
2026-09-15 16:20:16 +0800

    chore: version calendar drag release as 1.59.1

--- buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
+++ buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
@@ -5,8 +5,8 @@ object Base {
     const val namespace = "com.example.util.simpletimetracker"
 
     // Raise by 2 to account for wear version code.
-    const val versionCode = 145
-    const val versionName = "1.59"
+    const val versionCode = 146
+    const val versionName = "1.59.1"
     const val minSDK = 21
     const val currentSDK = 36
 
